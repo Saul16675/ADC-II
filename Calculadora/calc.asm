2f7ae565f0daafd4361745ce1f26d9f203f7d6a8
EsvinAbaj1 <89616184+EsvinAbaj1@users.noreply.github.com>
2021-08-31 13:17:51 -0600

    division

--- Calculadora/calc.asm
+++ Calculadora/calc.asm
@@ -1,67 +1,81 @@
 .model small
-.stack 64
+.stack 100
 .data
+msj1 db 'Numero 1: ','$'
+msj2 db 13,10,'Numero 2: ','$'
+msj3 db 13,10,'Suma: ','$'
 
-numero1 db 0
-numero2 db 0
-suma  db 0
-resta db 0
-multiplicacion db 0
-division db 0
-modulo db 0
-
-
-
-
-msgn1 db 10,13, "Ingrese el primer valor: ",'$'
-msgn2 db 10,13, "Ingrese el segundo valor: ",'$'
-msg1 db 10,13, "Suma= " , '$'
-msg2 db 10,13, "resta= " , '$'
-
-
+msj6 db 13,10,'Division: ','$'
+linea db 13,10,'$'
+var1 db 0
+var2 db 0
 .code
-begin proc far
+.startup
 
-mov ax, @data
-mov ds, ax
-
-mov ah,09
-lea dx, msgn1
+call limpia
+mov ah,09h
+lea dx, msj1 
 int 21h
-mov ah, 01
+
+call leer 
+sub al,30h 
+mov var1,al 
+mov ah,09h
+lea dx, msj2 
 int 21h
-sub al, 30h
-mov numero1, al
 
+call leer 
+sub al,30h 
+mov var2,al 
+mov bl,var2 
 
-mov ah,09
-lea dx, msgn2
+; SUMA
+add bl,var1 
+mov ah,09h
+lea dx,msj3 
 int 21h
-mov ah, 01
-int 21h
-sub al, 30h
-mov numero2, al
-
-
-mov al, numero1
-add al, numero2
-mov suma, al
-
-
-
-mov ah, 09
-lea dx, msg1
-int 21h
-mov dl, suma
-add dl, 30h
-mov ah, 02
+mov dl,bl 
+add dl,30h    
+mov ah,02h 
 int 21h
 
 
 
+;DIVISION
+mov ah,09h
+lea dx,msj6 
+int 21h
+xor ax,ax 
+mov al,var2
+mov bl,al
+mov al,var1
+div bl ;
+mov bl,al
+mov dl,bl
+add dl,30h
+mov ah,02h
+int 21h
+.exit
 
-mov ah, 4ch
-int 21h 
+; PROCEDIMIENTOS
+salto proc near
+mov ah,09h
+lea dx,linea
+int 21h
+mov dl,00h
+ret
+salto endp
 
+leer proc near
+mov ah,01h
+int 21h
+ret
+leer endp
 
-end begin
+limpia proc near
+mov ah,00h
+mov al,03h
+int 10h
+ret
+limpia endp
+end
